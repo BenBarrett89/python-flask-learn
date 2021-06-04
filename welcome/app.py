999377fde103c80bdf2f705a26e3951534dca258
Ben Barrett <benjamin.barrett@engineering.digital.dwp.gov.uk>
2021-06-04 17:13:26 +0100

    Up to video 8

--- welcome/app.py
+++ welcome/app.py
@@ -1,7 +1,7 @@
 #!python3
 
 # specify which libraries and modules to use (don't import all)
-from flask import Flask
+from flask import Flask, request, render_template
 
 # __name__ is something he doesn't explain
 app = Flask(__name__)
@@ -9,10 +9,18 @@ app = Flask(__name__)
 # @ is a decorator (of the below function) - provide additional functionality to an existing function
 @app.route('/')
 def root():
-  return 'This is my root page'
+  return render_template("index.html")
 
 @app.route('/bob')
 def bob():
   return 'Yo Bob! What\'s happening man?'
+
+# methods can be provided in another argument
+@app.route('/method', methods=['GET', 'POST'])
+def method():
+  if request.method == 'POST':
+    return "You've used a POST request"
+  else:
+    return "I reckon you're probably using a GET request!"
 
 app.run()
